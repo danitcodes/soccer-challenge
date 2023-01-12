cff58da2327087f8ac404be00fa2d3d9b916e574
Danielle Thompson <danithompson74@gmail.com>
2023-01-12 11:38:10 -0800

    Write passing test for list of teams w/pts

--- spec/game_results_spec.rb
+++ spec/game_results_spec.rb
@@ -21,12 +21,31 @@ describe('#game_results') do
   # tests sorting of the top 3 matchday teams
   # format the output printed to the terminal
 
-  it("gets the number of teams_in_league based on the unique count of team names") do
-    expect(teams_in_league_count(teams)).to(eq(6))
-  end
-
-  it("gets number of txt file lines per matchday based on # of teams in league") do
-    expect(lines_per_matchday(teams)).to(eq(3))
+  it("prints a list of teams with their corresponding points scored in a game") do
+    expect(teams).to(eq(["San Jose Earthquakes 3",
+                          "Santa Cruz Slugs 3\n",
+                          "Capitola Seahorses 1",
+                          "Aptos FC 0\n",
+                          "Felton Lumberjacks 2",
+                          "Monterey United 0\n",
+                          "Felton Lumberjacks 1",
+                          "Aptos FC 2\n",
+                          "Santa Cruz Slugs 0",
+                          "Capitola Seahorses 0\n",
+                          "Monterey United 4",
+                          "San Jose Earthquakes 2\n",
+                          "Santa Cruz Slugs 2",
+                          "Aptos FC 3\n",
+                          "San Jose Earthquakes 1",
+                          "Felton Lumberjacks 4\n",
+                          "Monterey United 1",
+                          "Capitola Seahorses 0\n",
+                          "Aptos FC 2",
+                          "Monterey United 0\n",
+                          "Capitola Seahorses 5",
+                          "San Jose Earthquakes 5\n",
+                          "Santa Cruz Slugs 1",
+                          "Felton Lumberjacks 1\n"]))
   end
 
   it("gets the names of the teams in a league") do
@@ -54,5 +73,13 @@ describe('#game_results') do
                                               "San Jose Earthquakes",
                                               "Santa Cruz Slugs",
                                               "Felton Lumberjacks"]))
+  end
+
+  it("gets the number of teams_in_league based on the unique count of team names") do
+    expect(teams_in_league_count(teams)).to(eq(6))
+  end
+
+  it("gets number of txt file lines per matchday based on # of teams in league") do
+    expect(lines_per_matchday(teams)).to(eq(3))
   end
 end
