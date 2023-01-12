5640160a8fbeb18d09ce0b41e2a1b27ffcda6069
Danielle Thompson <danithompson74@gmail.com>
2023-01-12 11:44:50 -0800

    Write passing test for games array output

--- spec/game_results_spec.rb
+++ spec/game_results_spec.rb
@@ -25,6 +25,21 @@ describe('#game_results') do
     expect(static_filename).to(eq("/Users/daniellethompson/soccer-challenge/sample-input.txt"))
   end
 
+  it("prints a list of games played to date") do
+    expect(games).to(eq(["San Jose Earthquakes 3, Santa Cruz Slugs 3\n",
+                        "Capitola Seahorses 1, Aptos FC 0\n",
+                        "Felton Lumberjacks 2, Monterey United 0\n",
+                        "Felton Lumberjacks 1, Aptos FC 2\n",
+                        "Santa Cruz Slugs 0, Capitola Seahorses 0\n",
+                        "Monterey United 4, San Jose Earthquakes 2\n",
+                        "Santa Cruz Slugs 2, Aptos FC 3\n",
+                        "San Jose Earthquakes 1, Felton Lumberjacks 4\n",
+                        "Monterey United 1, Capitola Seahorses 0\n",
+                        "Aptos FC 2, Monterey United 0\n",
+                        "Capitola Seahorses 5, San Jose Earthquakes 5\n",
+                        "Santa Cruz Slugs 1, Felton Lumberjacks 1\n"]))
+  end
+
   it("prints a list of teams with their corresponding points scored in a game") do
     expect(teams).to(eq(["San Jose Earthquakes 3",
                           "Santa Cruz Slugs 3\n",
